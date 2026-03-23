72258121b45efb1197592b0a0973d76b2c44d33f
troyhunterz <chatgptacc0unt1@gmail.com>
2026-03-23 15:50:03 +0100

    fix test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,9 +6,9 @@ client = TestClient(app)
 
 
 def test_root():
-    response = client.get('/')
+    response = client.get('/health')
     assert response.status_code == 200
-    assert response.json()['status'] == 'ok'
+    assert response.json()['status'] == 'healthy'
 
 
 def test_health():
